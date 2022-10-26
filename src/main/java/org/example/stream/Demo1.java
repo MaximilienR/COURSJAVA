39cf7ca95d462f4fa91dbd20c7b4ce3727005acd
Maximilien Roussel <ro.maximilien@gmail.com>
2022-10-26 16:28:50 +0200

    update

--- src/main/java/org/example/stream/Demo1.java
+++ src/main/java/org/example/stream/Demo1.java
@@ -1,6 +1,7 @@
 package org.example.stream;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 public class Demo1 {
@@ -8,7 +9,24 @@ public class Demo1 {
 
     public static void main(){
         List<String> prenoms = Arrays.asList("abdallah","benoit","albert","thierry","alain","jean","alban","alain");
-        prenoms.stream().filter(p->p.startsWith("a"))
+        prenoms.stream()
+                .filter(p->p.startsWith("a"))
                 .forEach(System.out::println);
+        System.out.println("count :");
+        System.out.println("le nombre de personne dont le prenom commence par un a est de :"+prenoms.stream()
+                .filter(p->p.startsWith("a")).count());
+
+        System.out.println();
+        System.out.println("distinct :");
+        prenoms.stream().distinct().forEach(System.out::println);
+
+        System.out.println("Limit :");
+        prenoms.stream().limit(4).forEach(System.out::println);
+
+        System.out.println("sorted");
+
+        prenoms.stream().sorted().forEach(System.out::println);
+        prenoms.stream().sorted(Comparator.reverseOrder()).forEach(System.out::println);
+
     }
 }
