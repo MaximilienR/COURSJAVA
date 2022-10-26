e3e7f21ddef53c93cf2cbcad7ada4869e19cd270
Maximilien Roussel <ro.maximilien@gmail.com>
2022-10-26 10:28:31 +0200

    update

--- src/main/java/org/example/exception/SimpleExemple.java
+++ src/main/java/org/example/exception/SimpleExemple.java
@@ -1,5 +1,6 @@
 package org.example.exception;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class SimpleExemple {
@@ -8,23 +9,27 @@ public class SimpleExemple {
 
         System.out.println("***** division *****");
         Scanner scanner=new Scanner(System.in);
-        System.out.println("veuillez saisir un nombre");
-        Integer valeur = scanner.nextInt();
-        System.out.println("veuillez entrer un diviseur");
-        Integer diviseur = scanner.nextInt();
+
 
         try {
+            System.out.println("veuillez saisir un nombre");
+            Integer valeur = scanner.nextInt();
+            System.out.println("veuillez entrer un diviseur");
+            Integer diviseur = scanner.nextInt();
             double resultat=valeur/diviseur;
             System.out.println("resultat :"+resultat);
+        }catch (ArithmeticException |InputMismatchException e) {
+          e.printStackTrace();
+          SimpleExemple.main();
+            //  System.out.println("Exception"+e.getStackTrace());
+          /*  System.out.println("on ne peut pas diviser par 0");
+            System.out.println("tu recommence...");
+            SimpleExemple.main();
+        }catch(InputMismatchException e) {
+            System.out.println("tu dois saisir des entiers");*/
         }catch (Exception e){
-
-        System.out.println("on ne peut pas diviser par 0");
+            System.out.println("c'est une exception");
+        }
 
     }
-
-
-
-
-
-}
 }
