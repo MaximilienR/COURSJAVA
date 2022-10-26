4dc47f067aa4fb9f5622dd48f7088c7b5e1e18bd
Maximilien Roussel <ro.maximilien@gmail.com>
2022-10-26 16:53:00 +0200

    update

--- src/main/java/org/example/stream/Demo1.java
+++ src/main/java/org/example/stream/Demo1.java
@@ -1,8 +1,8 @@
 package org.example.stream;
 
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
+
+import static org.example.methode.MethodeParam.sum;
 
 public class Demo1 {
 
@@ -28,5 +28,29 @@ public class Demo1 {
         prenoms.stream().sorted().forEach(System.out::println);
         prenoms.stream().sorted(Comparator.reverseOrder()).forEach(System.out::println);
 
+        List<Employe>liste=new ArrayList<>();
+
+        liste.add(new Employe(1,Genre.HOMME,176,2900));
+        liste.add(new Employe(2,Genre.HOMME,186,2000));
+        liste.add(new Employe(3,Genre.HOMME,180,1900));
+        liste.add(new Employe(4,Genre.FEMME,150,1000));
+        liste.add(new Employe(5,Genre.FEMME,150,1300));
+
+        double totalSalaire =liste.stream()
+        .mapToDouble(e->e.getSalaire())
+        .sum();
+
+        System.out.println("titak sakaure ; "+totalSalaire);
+
+        Optional<Employe> employe =liste.stream()
+        .filter(p->p.getTaille()>=176)
+                .findFirst();
+
+        System.out.println("le premier employe qui fait au moins 176 est"+employe.get());
+
+        int []nbr={1,2,3,5,4,7};
+        int somme=Arrays.stream(nbr).reduce(0,(a,b)->a+b);
+
+        System.out.println("la somme du tableau nrb est de "+ somme);
     }
 }
---
+++ src/main/java/org/example/stream/Genre.java
@@ -0,0 +1,6 @@
+package org.example.stream;
+
+public enum Genre {
+    HOMME,
+    FEMME
+}
