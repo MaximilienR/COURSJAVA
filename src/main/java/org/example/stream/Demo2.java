9b9246530012896916f4b8db1868cbae8f6ec2a9
Maximilien Roussel <ro.maximilien@gmail.com>
2022-10-27 09:21:14 +0200

    update

--- src/main/java/org/example/stream/Demo2.java
+++ src/main/java/org/example/stream/Demo2.java
@@ -11,8 +11,8 @@ public class Demo2 {
   System.out.println("--Demo : fillter,map et collect");
   List<Product> products = new ArrayList<>(Arrays.asList(
           new Product("iphone",1000,12456),
-          new Product("table",500,78982),
-          new Product("pull",74,79472),
+          new Product("table",500,789821),
+          new Product("pull",74,794722),
           new Product("tele",2000,987456),
           new Product("jeu",20,944716)
           ));
@@ -25,6 +25,29 @@ public class Demo2 {
                   .collect(Collectors.toList());
 
           listModifie.stream().forEach(System.out::println);
+
+         System.out.println("demo : toMap");
+
+         Cart cart1=new Cart();
+         Cart cart2=new Cart();
+         Cart cart3=new Cart();
+
+         cart1.setProductLIST(Arrays.asList(products.get(0)));
+         cart2.setProductLIST(Arrays.asList(products.get(1), products.get(2)));
+         cart3.setProductLIST(Arrays.asList(products.get(3),products.get(4)));
+
+        List<Cart>carts=new ArrayList<>(Arrays.asList(cart1,cart2,cart3));
+
+        Product[] products1=carts.stream()
+                .flatMap(cart -> cart.getProductLIST().stream())
+                .filter(product -> product.getReference()>101100)
+                .map(product -> {
+                    product.setPrice(product.getPrice() * 1.50);
+                    return product;
+                }).toArray(Product[]::new);
+
+        Arrays.stream(products1).forEach(System.out::println);
+
 
  }
 
